3f99a9b01cfc19fab9a1fe849199aa330874d8d0
Johan Tillman <johan123tillman@gmail.com>
2025-12-02 08:41:09 +0100

    fedora adaptations

--- lua/johan/core/keymaps.lua
+++ lua/johan/core/keymaps.lua
@@ -6,45 +6,54 @@ local keymap = vim.keymap
 keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-keymap.set({"n", "v"}, "<leader>y", [["+y]])
+keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 keymap.set("n", "<leader>Y", [["+Y]])
 
 keymap.set("n", "<C-d>", "<C-d>zz")
 keymap.set("n", "<C-u>", "<C-u>zz")
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap.set("n", "<A-left>", "<C-o>")
+keymap.set("n", "<A-right>", "<C-i>")
 
 -- window management
 keymap.set("n", "<leader>wv", "<C-w>v", {
-    desc = "Split window vertically"
+	desc = "Split window vertically",
 }) -- split window vertically
 keymap.set("n", "<leader>wh", "<C-w>s", {
-    desc = "Split window horizontally"
+	desc = "Split window horizontally",
 }) -- split window horizontally
 keymap.set("n", "<leader>we", "<C-w>=", {
-    desc = "Make splits equal size"
+	desc = "Make splits equal size",
 }) -- make split windows equal width & height
 keymap.set("n", "<leader>wd", "<cmd>close<CR>", {
-    desc = "Close current split"
+	desc = "Close current split",
 }) -- close current split window
 
 -- save file
 keymap.set("n", "<leader>fs", "<cmd>:w<CR>", {
-    desc = "Save file"
+	desc = "Save file",
 })
 -- quit nvim
 keymap.set("n", "<leader>qq", "<cmd>:q<CR>", {
-    desc = "Quit nvim"
+	desc = "Quit nvim",
 })
 
 -- buffers
-keymap.set("n", "<A-right>", "<C-o>", {
-    desc = "Previous buffer"
-})
-keymap.set("n", "<A-left>", "<C-i>", {
-    desc = "Next buffer"
-})
 keymap.set("n", "<leader>nh", "<CMD>:nohl<CR>", {
-    desc = "Remove search highlights"
+	desc = "Remove search highlights",
 })
 keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+keymap.set(
+	"n",
+	"<leader>tt",
+	"<cmd>:lua vim.diagnostic.config({virtual_text=false})<CR>",
+	{ desc = "Disable virtual text" }
+)
+keymap.set(
+	"n",
+	"<leader>te",
+	"<cmd>:lua vim.diagnostic.config({virtual_text=true})<CR>",
+	{ desc = "Enable virtual text" }
+)
--- lua/johan/plugins/fzf-lua.lua
+++ lua/johan/plugins/fzf-lua.lua
@@ -1,26 +1,42 @@
 return {
-    "ibhagwan/fzf-lua",
-    -- optional for icon support
-    dependencies = {"nvim-tree/nvim-web-devicons"},
-    -- or if using mini.icons/mini.nvim
-    -- dependencies = { "nvim-mini/mini.icons" },
-    --   opts = {}
-    config = function()
-        keymap = vim.keymap
-        keymap.set("n", "<leader>ff", "<cmd>FzfLua files<CR>", {
-            desc = "Files"
-        })
-        keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<CR>", {
-            desc = "Live grep"
-        })
-        keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<CR>", {
-            desc = "Buffers"
-        })
-        keymap.set("n", "<leader>fc", "<cmd>FzfLua grep_cword<CR>", {
-            desc = "Grep word under cursor"
-        })
-        keymap.set("n", "<leader>fr", "<cmd>FzfLua oldfiles<CR>", {
-            desc = "Recent files"
-        })
-    end
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- or if using mini.icons/mini.nvim
+	-- dependencies = { "nvim-mini/mini.icons" },
+	--   opts = {}
+	config = function()
+		keymap = vim.keymap
+		keymap.set("n", "<leader>ff", "<cmd>FzfLua files<CR>", {
+			desc = "Files",
+		})
+		keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<CR>", {
+			desc = "Live grep",
+		})
+		keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<CR>", {
+			desc = "Buffers",
+		})
+		keymap.set("n", "<leader>fr", "<cmd>FzfLua oldfiles<CR>", {
+			desc = "Show recent files",
+		})
+		keymap.set("n", "<leader>fc", "<cmd>FzfLua grep_cword<CR>", {
+			desc = "Grep word under cursor",
+		})
+		keymap.set("n", "<leader>ss", "<cmd>FzfLua blines<CR>", {
+			desc = "Grep word under cursor",
+		})
+
+		keymap.set("n", "gr", "<cmd>FzfLua lsp_references<CR>", {
+			desc = "Find references",
+		})
+		keymap.set("n", "gd", "<cmd>FzfLua lsp_definitions<CR>", {
+			desc = "Go to definition",
+		})
+		keymap.set("n", "gD", "<cmd>FzfLua lsp_declarations<CR>", {
+			desc = "Show declarations",
+		})
+		keymap.set("n", "<leader>ji", "<cmd>FzfLua lsp_document_symbols<CR>", {
+			desc = "Show symbol outline",
+		})
+	end,
 }
